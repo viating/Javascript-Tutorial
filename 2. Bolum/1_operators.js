edfc81b62a5ac6fb52c5da2c2aea3e88f55817c6
viating <alibayar028@gmail.com>
2023-03-07 23:10:20 +0300

    a

---
+++ 2. Bolum/1_operators.js
@@ -0,0 +1,7 @@
+// Eşittir operatörü
+let a = 5
+console.log(a);
+
+// Matematik Operatörleri
+a = 3 + 5 - 2 * 1 / 3
+console.log(a);
---
+++ 2. Bolum/2_operators_devam.js
@@ -0,0 +1,9 @@
+// Matematik Operatörleri
+let a = 5;
+a++
+console.log(a)
+a--
+console.log(a)
+console.log(a**2)
+console.log(a/2)
+console.log(a%2)
