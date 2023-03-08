340e8cf968b43b92b22978425644e91fcda8d284
viating <alibayar028@gmail.com>
2023-03-08 19:39:49 +0300

    2. Bolum

---
+++ 2. Bolum/3_operators_devam.js
@@ -0,0 +1,10 @@
+// Matematik Operatörleri
+let a = 3 == 5
+let b = 3 != 5
+let c = 3 > 5
+let d = 3 <= 5
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
---
+++ 2. Bolum/6_veri_tipleri.js
@@ -0,0 +1,30 @@
+let metin = "Viating"
+console.log( typeof metin)
+
+let sayı = 123
+console.log( typeof sayı)
+
+let buyuk_sayi = 1231848511849845168946549815644519819865
+console.log( typeof buyuk_sayi)
+
+let x = BigInt("1231848511849845168946")
+console.log( typeof x)
+
+let boolaen = true
+console.log(typeof boolaen)
+
+let nesne = {
+                adi : "H",
+                soyadi : "G",
+                dene : function(a)
+                        {
+                            return a+5
+                        }
+            }
+console.log(typeof nesne)
+
+let dizi = ["Ali", "Veli", "Zeki"]
+console.log(typeof dizi)
+
+const tarih = new Date("2023-03-02")
+console.log(typeof tarih)
