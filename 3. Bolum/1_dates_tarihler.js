a2f633b16746c96da6219a47ab7e2864d49b99ed
viating <alibayar028@gmail.com>
2023-03-23 20:24:54 +0300

    update

---
+++ 3. Bolum/1_dates_tarihler.js
@@ -0,0 +1,7 @@
+//const yeni_tarih = new Date("20.03.2023"); <= Yanlış bir tanımlamadır.
+
+var yeni_tarih = new Date("2023-03-25");
+console.log(yeni_tarih);
+
+yeni_tarih = new Date("March 25, 2023");
+console.log(yeni_tarih);
---
+++ 3. Bolum/2_dates.js
@@ -0,0 +1,8 @@
+yeni_tarih = new Date("March 25, 2023 10:22:22");
+console.log(yeni_tarih);
+
+//Tarihin sadece yılını yazdırma
+console.log(yeni_tarih.getFullYear());
+
+//Tarihin sadece saatini yazdırma
+console.log(yeni_tarih.getHours());
